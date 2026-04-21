7815e825532de06c2c91c8984d061e6f43608f3f
Aksh Naik <akshnaik1907@gmail.com>
2026-04-21 22:25:34 +0530

    Redeploy for env issues .

--- server.js
+++ server.js
@@ -265,8 +265,9 @@ Knowledge: ${knowledge}`
   }
 });
 
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
